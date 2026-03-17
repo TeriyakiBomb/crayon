8e1d8aa1487dae47cb83e7daf9880b92c9fe17f5
Ash Joseph <kamikazedesigns@gmail.com>
2026-03-17 22:33:17 +0000

    Configure that chap

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Flex from "./components/flex.svelte";
+</script>
+
+<main class="container py-10 gap-y-4">
+  <Flex />
+</main>
+
+<style lang="scss">
+  @use "crayon";
+
+  h1 {
+    color: crayon.color("blue-950");
+  }
+</style>
