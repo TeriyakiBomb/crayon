a33381fa2f28a4cc8f4ec321f20189db5d833a6b
Ash Joseph <kamikazedesigns@gmail.com>
2026-03-17 23:52:37 +0000

    Added a logo

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,17 @@ import Flex from "./components/flex.svelte";
 </script>
 
 <main class="container py-10 gap-y-4">
+  <img src="/logo-color.svg" alt="Crayon" class="logo" />
   <Flex />
 </main>
 
 <style lang="scss">
   @use "crayon";
+
+  .logo {
+    height: crayon.size(20);
+    width: auto;
+  }
 
   h1 {
     color: crayon.color("blue-950");
